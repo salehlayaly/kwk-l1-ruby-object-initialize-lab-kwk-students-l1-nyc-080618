5d7dbb54a4ad12b279ac01404576d0b34b4e4ff0
salehlayaly <lsaleh@hcstonline.org>
2018-08-10 16:30:51 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,9 @@
 # dog.rb
+  class Dog 
+    def initialize(name, breed = "Mutt") 
+      @name = name 
+      @breed = breed
+    end 
+  end
+  
+  dog = Dog.new("name") 
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,8 @@
 # person.rb
+class Person 
+  def initialize(name) 
+    @name = name 
+  end 
+end
+
+person_one = Person.new("name")
