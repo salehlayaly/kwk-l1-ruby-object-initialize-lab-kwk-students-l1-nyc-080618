fd81d383c5a010849ec8edc196da01a9b4fab153
salehlayaly <lsaleh@hcstonline.org>
2018-08-10 16:28:24 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,17 @@
 # person.rb
+class Person 
+  def initialize(name) 
+    @name = name 
+  end 
+end
+
+person_one = Person.new("name")
+  
+  class Dog 
+    def initialize(name, breed = "Mutt") 
+      @name = name 
+      @breed = breed
+    end 
+  end
+  
+  dog = Dog.new(name) 
